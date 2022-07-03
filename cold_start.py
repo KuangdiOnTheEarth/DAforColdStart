339f1cc8136e145e9701657ac885237d8d042241
KuangdiOnTheEarth <44149122+KuangdiOnTheEarth@users.noreply.github.com>
2022-07-03 16:46:11 +0100

    Add script for split user-cs, item-cs, mixed-cs, ws test sets.

---
+++ cold_start.py
@@ -0,0 +1,102 @@
+import argparse
+from collections import defaultdict
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', required=True)
+parser.add_argument('--raw_dataset')
+parser.add_argument('--cs_dataset')
+
+def get_cold_user_item(fname, cs_user_prop=0.2, cs_item_prop=0.2):
+    user_count = {}  # number of interactions for each user
+    item_count = {}  # number of interactions on each item
+    User = defaultdict(list)  # the list of interacted items for each user
+    Item = defaultdict(list)  # the list of users who interacted with each item
+    ucs_seq = {}  # test set for evaluating user cold-start
+    ics_seq = {}  # test set for evaluating item cold-start
+    mcs_seq = {}  # test set for evaluating mixed cold-start cases: cold-start user interacted with cold-start items
+    ws_seq = {}   # test set for evaluating warm-start cases
+
+    f = open('data/%s.txt' % fname, 'r')
+    for line in f:
+        u, i = line.rstrip().split(' ')
+        u = int(u)
+        i = int(i)
+        User[u].append(i)
+        Item[i].append(u)
+    num_user = len(User)
+    num_item = len(Item)
+
+    sorted_user_list = sorted(User, key=lambda key: len(User[key]))  # user-ids sorted by # interactions from he/she
+    sorted_item_list = sorted(Item, key=lambda key: len(Item[key]))  # item-ids sorted by # interactions on it
+    cs_user_list = sorted_user_list[:int(num_user * cs_user_prop)]
+    cs_item_list = sorted_item_list[:int(num_item * cs_item_prop)]
+
+    print("Users:")
+    print("total users:" + str(num_user) +
+          "; # interactions between: " + str(len(User[sorted_user_list[0]])) + " ~ " + str(
+        len(User[sorted_user_list[-1]])))
+
+    # find out training samples that are both user & item cold-start
+    cs_item_seq_set = set()
+    for iid in cs_item_list:
+        user_set = set(Item[iid])
+        cs_item_seq_set = cs_item_seq_set.union(user_set)
+    mixed_set = set(cs_user_list).intersection(cs_item_seq_set)
+    for uid in mixed_set:
+        mcs_seq[uid] = User.pop(uid)
+        if uid == 3822: print("3822 discovered")
+
+    # collect the cold-start users (with least iterations)
+    ucs_max, ucs_min = 0, 10 ** 6
+    for uid in set(cs_user_list).difference(mixed_set):
+        ucs_seq[uid] = User.pop(uid)
+        ucs_max, ucs_min = max(ucs_max, len(ucs_seq[uid])), min(ucs_min, len(ucs_seq[uid]))
+
+    # collect the cold-start items (with least iterations)
+    ics_max, ics_min = 0, 10**6
+    for uid in cs_item_seq_set.difference(mixed_set):
+        # sequence cut-off after the last cold-start item -> allow test on cold-start item
+        seq = User.pop(uid)
+        i = len(seq) - 1
+        while seq[i] not in cs_item_list: i -= 1
+        if i > ucs_max:     # all item-cold-start sample should be non-user-cold-start
+            ics_seq[uid] = seq[0:i+1]
+            ics_max, ics_min = max(ics_max, len(ics_seq[uid])), min(ics_min, len(ics_seq[uid]))
+
+    # the remaining sequences are for warm-start case
+    ws_seq = User
+
+    print("cold-start users: " + str(len(cs_user_list)))
+    print("without overlap: " + str(len(ucs_seq)) +
+          "; sequence length between: " + str(ucs_min) + " ~ " + str(ucs_max))
+
+    print("\nItems:")
+    print("total items:" + str(num_item) +
+          "; # interacted users between: " + str(len(Item[sorted_item_list[0]])) + " ~ " + str(len(Item[sorted_item_list[-1]])))
+    print("cold-start items: " + str(len(cs_item_list)) +
+          "; # interacted users between: " + str(len(Item[cs_item_list[0]])) + " ~ " + str(len(Item[cs_item_list[-1]])))
+    print("# sequences for item-cold-start: " + str(len(cs_item_seq_set)))
+    print("without overlap: " + str(len(ics_seq)) +
+          "; sequences length between: " + str(ics_min) + " ~ " + str(ics_max))
+
+    print("\nMixed:")
+    print("samples that are both cold-start user and item: " + str(len(mixed_set)))
+
+
+    # for user in User:
+    #     nfeedback = len(User[user])
+    #     if nfeedback < 3:
+    #         user_train[user] = User[user]
+    #         user_valid[user] = []
+    #         user_test[user] = []
+    #     else:
+    #         user_train[user] = User[user][:-2]
+    #         user_valid[user] = []
+    #         user_valid[user].append(User[user][-2])
+    #         user_test[user] = []
+    #         user_test[user].append(User[user][-1])
+    # return [user_train, user_valid, user_test, usernum, itemnum]
+
+
+if __name__ == '__main__':
+    get_cold_user_item("ml-1m", cs_user_prop=0.2, cs_item_prop=0.1)
